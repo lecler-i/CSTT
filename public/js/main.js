eb2f8b4d72f3ce4c428eb98f456e78ee724cd122
lecler_i <thomas.leclercq@epitech.eu>
2014-05-24 02:58:15 +0200

    Ok js clean

--- public/js/main.js
+++ public/js/main.js
@@ -1,59 +1,44 @@
 
 
   function enterFullscreen(element) {
-    if(element.requestFullScreen) {
+    if (element.requestFullScreen) {
       element.requestFullScreen();
-    } else if(element.webkitRequestFullScreen) {
+    } else if (element.webkitRequestFullScreen) {
       element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-    } else if(element.mozRequestFullScreen){
+    } else if (element.mozRequestFullScreen){
       element.mozRequestFullScreen();
     } else {
       alert('Votre navigateur ne supporte pas le mode plein écran, il est temps de passer à un plus récent ;)');
     }
  }
 
-function draw_sky(canvas)
-{
+function drawSky(canvas) {
     var context = canvas.getContext("2d");
 
     context.fillStyle = "#000";
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     context.fillStyle = "#FFF";
-    context.fillRect(canvas.width / 2, 50, 10, 10);
+    context.fillRect(canvas.width / 2, canvas.height / 2, 10, 10);
 }
 
-window.onresize = function()
-{
+window.onresize = function() {
   var canvas = document.getElementById("spaceCanvas");
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   console.log(canvas.width);
-  draw_sky(canvas);
-}
-
-var callback = function(e) {
-  var state = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
-  var canvas = document.getElementById("spaceCanvas");
-
-  if (state)
-  {
-    canvas.style.display = "block";
-  }
-  else
-  {
-    canvas.style.display = "none";
-  }
+  drawSky(canvas);
 };
 
+var fullScreenCallback = function(e) {
+  var state = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
+  var canvas = document.getElementById("spaceCanvas");
+  canvas.style.display = state ? "block" : "none";
+};
 
-
-document.addEventListener("fullscreenchange", callback);
-document.addEventListener("webkitfullscreenchange", callback);
+document.addEventListener("fullscreenchange", fullScreenCallback);
+document.addEventListener("webkitfullscreenchange", fullScreenCallback);
  
-
-
-
 document.getElementById("startButton").onclick = function () {
     var canvas = document.getElementById("spaceCanvas");
     enterFullscreen(canvas);
